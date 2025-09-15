2f82388f7a9579ae4e7613ff5f99f9086d59aac2
Saint <zackryan512@gnail.com>
2025-09-16 01:02:13 +0700

    More changes.

--- src/main/java/com/leon/saintsdragons/common/registry/ModSounds.java
+++ src/main/java/com/leon/saintsdragons/common/registry/ModSounds.java
@@ -16,38 +16,38 @@ public final class ModSounds {
             DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, SaintsDragons.MOD_ID);
 
     // ===== AMBIENT SOUNDS =====
-    public static final RegistryObject<SoundEvent> DRAGON_GRUMBLE_1 = registerSound("dragon_grumble_1");
-    public static final RegistryObject<SoundEvent> DRAGON_GRUMBLE_2 = registerSound("dragon_grumble_2");
-    public static final RegistryObject<SoundEvent> DRAGON_GRUMBLE_3 = registerSound("dragon_grumble_3");
+    public static final RegistryObject<SoundEvent> DRAGON_GRUMBLE_1 = registerSound("lightningdragon_grumble_1");
+    public static final RegistryObject<SoundEvent> DRAGON_GRUMBLE_2 = registerSound("lightningdragon_grumble_2");
+    public static final RegistryObject<SoundEvent> DRAGON_GRUMBLE_3 = registerSound("lightningdragon_grumble_3");
 
-    public static final RegistryObject<SoundEvent> DRAGON_PURR = registerSound("dragon_purr");
-    public static final RegistryObject<SoundEvent> DRAGON_SNORT = registerSound("dragon_snort");
-    public static final RegistryObject<SoundEvent> DRAGON_CHUFF = registerSound("dragon_chuff");
+    public static final RegistryObject<SoundEvent> DRAGON_PURR = registerSound("lightningdragon_purr");
+    public static final RegistryObject<SoundEvent> DRAGON_SNORT = registerSound("lightningdragon_snort");
+    public static final RegistryObject<SoundEvent> DRAGON_CHUFF = registerSound("lightningdragon_chuff");
 
     // ===== EMOTIONAL SOUNDS =====
-    public static final RegistryObject<SoundEvent> DRAGON_CONTENT = registerSound("dragon_content");
-    public static final RegistryObject<SoundEvent> DRAGON_ANNOYED = registerSound("dragon_annoyed");
-    public static final RegistryObject<SoundEvent> DRAGON_EXCITED = registerSound("dragon_excited");
+    public static final RegistryObject<SoundEvent> DRAGON_CONTENT = registerSound("lightningdragon_content");
+    public static final RegistryObject<SoundEvent> DRAGON_ANNOYED = registerSound("lightningdragon_annoyed");
+    public static final RegistryObject<SoundEvent> DRAGON_EXCITED = registerSound("lightningdragon_excited");
 
     // ===== COMBAT SOUNDS =====
-    public static final RegistryObject<SoundEvent> DRAGON_ROAR = registerSound("dragon_roar");
-    public static final RegistryObject<SoundEvent> DRAGON_SUMMON_STORM = registerSound("summon_storm");
-    public static final RegistryObject<SoundEvent> DRAGON_GROWL_WARNING = registerSound("dragon_growl_warning");
-    public static final RegistryObject<SoundEvent> DRAGON_HURT = registerSound("dragon_hurt");
-    public static final RegistryObject<SoundEvent> DRAGON_BITE = registerSound("dragon_bite");
-    public static final RegistryObject<SoundEvent> DRAGON_HORNGORE = registerSound("dragon_horngore");
-    public static final RegistryObject<SoundEvent> DRAGON_DIE = registerSound("dragon_die");
+    public static final RegistryObject<SoundEvent> DRAGON_ROAR = registerSound("lightningdragon_roar");
+    public static final RegistryObject<SoundEvent> DRAGON_SUMMON_STORM = registerSound("lightningdragon_summon_storm");
+    public static final RegistryObject<SoundEvent> DRAGON_GROWL_WARNING = registerSound("lightningdragon_growl_warning");
+    public static final RegistryObject<SoundEvent> DRAGON_HURT = registerSound("lightningdragon_hurt");
+    public static final RegistryObject<SoundEvent> DRAGON_BITE = registerSound("lightningdragon_bite");
+    public static final RegistryObject<SoundEvent> DRAGON_HORNGORE = registerSound("lightningdragon_horngore");
+    public static final RegistryObject<SoundEvent> DRAGON_DIE = registerSound("lightningdragon_die");
 
     // ===== STEP SOUNDS =====
     public static final RegistryObject<SoundEvent> DRAGON_STEP = registerSound("dragon_step") ;
 
     // ===== KEYFRAMED ANIMATION SOUNDS =====
-    public static final RegistryObject<SoundEvent> FLAP1 = registerSound("flap1");
-    public static final RegistryObject<SoundEvent> STEP1 = registerSound("step1");
-    public static final RegistryObject<SoundEvent> STEP2 = registerSound("step2");
+    public static final RegistryObject<SoundEvent> FLAP1 = registerSound("lightningdragon_flap1");
+    public static final RegistryObject<SoundEvent> STEP1 = registerSound("lightningdragon_step1");
+    public static final RegistryObject<SoundEvent> STEP2 = registerSound("lightningdragon_step2");
     // Run steps (faster cadence, distinct audio)
-    public static final RegistryObject<SoundEvent> RUN_STEP1 = registerSound("run_step1");
-    public static final RegistryObject<SoundEvent> RUN_STEP2 = registerSound("run_step2");
+    public static final RegistryObject<SoundEvent> RUN_STEP1 = registerSound("lightningdragon_run_step1");
+    public static final RegistryObject<SoundEvent> RUN_STEP2 = registerSound("lightningdragon_run_step2");
 
     /**
      * Helper method to register sounds without writing the same crap over and over
