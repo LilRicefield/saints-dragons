bd809da0a876f4c21e96e60392e9a104ba748d06
Saint <zackryan512@gnail.com>
2025-09-17 00:05:24 +0700

    Refactored the lightning dragon

--- src/main/java/com/leon/saintsdragons/server/entity/dragons/lightningdragon/handlers/LightningDragonInteractionHandler.java
+++ src/main/java/com/leon/saintsdragons/server/entity/dragons/lightningdragon/handlers/LightningDragonInteractionHandler.java
@@ -8,7 +8,6 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 
 /**
  * Handles all player interactions with Lightning Dragons.
