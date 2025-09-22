23b5ff23141982d7d77e80ac4a4d26c10b47d21f
Saint <zackryan512@gnail.com>
2025-09-22 22:08:09 +0700

    Bug fixing for states

--- src/main/java/com/leon/saintsdragons/common/network/DragonAnimTickets.java
+++ src/main/java/com/leon/saintsdragons/common/network/DragonAnimTickets.java
@@ -11,8 +11,6 @@ public final class DragonAnimTickets {
     public static final SerializableDataTicket<Integer> GROUND_STATE = registerInt("dragon_ground_state");
     public static final SerializableDataTicket<Integer> FLIGHT_MODE = registerInt("dragon_flight_mode");
 
-    private DragonAnimTickets() {
-    }
 
     public static void bootstrap() {
         // Force class loading to ensure tickets are registered on both logical sides
