bff8695dee7154f47df9f54da385f2c263dd3a3e
Saint <zackryan512@gnail.com>
2025-10-05 08:29:59 +0700

    Repath

--- src/main/java/com/leon/saintsdragons/server/entity/conductivity/ElectricalConductivityHelper.java
+++ src/main/java/com/leon/saintsdragons/server/entity/conductivity/ElectricalConductivityHelper.java
@@ -1,5 +1,6 @@
 package com.leon.saintsdragons.server.entity.conductivity;
 
+import com.leon.saintsdragons.server.entity.interfaces.ElectricalConductivityCapable;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
