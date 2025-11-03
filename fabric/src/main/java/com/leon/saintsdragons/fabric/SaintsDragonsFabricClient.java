79f383e26b708b2f5cd3d7d0d2cddf5446e48d62
Saint <zackryan512@gnail.com>
2025-11-03 14:01:25 +0700

    Fabric Dragon Ui wired.

--- fabric/src/main/java/com/leon/saintsdragons/fabric/SaintsDragonsFabricClient.java
+++ fabric/src/main/java/com/leon/saintsdragons/fabric/SaintsDragonsFabricClient.java
@@ -3,6 +3,7 @@ package com.leon.saintsdragons.fabric;
 import com.leon.saintsdragons.client.ClientProxy;
 import com.leon.saintsdragons.client.init.CommonClientModEvents;
 import com.leon.saintsdragons.fabric.client.FabricDragonRideKeybinds;
+import com.leon.saintsdragons.fabric.client.FabricDragonUI;
 import com.leon.saintsdragons.fabric.client.event.FabricClientEventHandler;
 import com.leon.saintsdragons.fabric.client.particle.FabricParticleRegistry;
 import net.fabricmc.api.ClientModInitializer;
@@ -15,6 +16,7 @@ public final class SaintsDragonsFabricClient implements ClientModInitializer {
         CommonClientModEvents.registerEntityRenderers(EntityRendererRegistry::register);
         FabricParticleRegistry.registerParticleFactories();
         FabricDragonRideKeybinds.init();
+        FabricDragonUI.init();
         FabricClientEventHandler.init();
         new ClientProxy().clientInit();
     }
