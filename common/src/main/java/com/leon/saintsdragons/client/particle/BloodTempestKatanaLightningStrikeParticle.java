37cb19b1c6c1e579d05fdb9518149e09647591bb
Saint <zackryan512@gmail.com>
2026-09-05 22:23:27 +0700

    Hell Yeah

--- common/src/main/java/com/leon/saintsdragons/client/particle/BloodTempestKatanaLightningStrikeParticle.java
+++ common/src/main/java/com/leon/saintsdragons/client/particle/BloodTempestKatanaLightningStrikeParticle.java
@@ -44,8 +44,6 @@ public final class BloodTempestKatanaLightningStrikeParticle extends TextureShee
 
     @Override
     public void render(VertexConsumer buffer, Camera camera, float partialTicks) {
-        updateSprite(partialTicks);
-
         Vec3 cameraPos = camera.getPosition();
         double worldX = Mth.lerp(partialTicks, this.xo, this.x);
         double worldY = Mth.lerp(partialTicks, this.yo, this.y);
@@ -88,19 +86,36 @@ public final class BloodTempestKatanaLightningStrikeParticle extends TextureShee
                 z + rightZ * HALF_WIDTH
         );
 
+        int light = this.getLightColor(partialTicks);
+        float animationFrame = Math.max(0.0F, this.age + partialTicks) / FRAME_DURATION_TICKS;
+        int currentFrame = Math.min((int)Math.floor(animationFrame), FRAME_COUNT - 1);
+        int nextFrame = Math.min(currentFrame + 1, FRAME_COUNT - 1);
+        float blend = nextFrame == currentFrame ? 0.0F : smoothStep(animationFrame - currentFrame);
+
+        this.setSprite(this.sprites.get(currentFrame, FRAME_COUNT - 1));
+        renderFrame(buffer, bottomLeft, topLeft, topRight, bottomRight, light,
+                this.alpha * (1.0F - blend));
+        if (nextFrame != currentFrame && blend > 0.001F) {
+            this.setSprite(this.sprites.get(nextFrame, FRAME_COUNT - 1));
+            renderFrame(buffer, bottomLeft, topLeft, topRight, bottomRight, light,
+                    this.alpha * blend);
+        }
+    }
+
+    private void renderFrame(VertexConsumer buffer, Vector3f bottomLeft, Vector3f topLeft,
+                             Vector3f topRight, Vector3f bottomRight, int light, float alpha) {
         float u0 = this.getU0();
         float u1 = this.getU1();
         float v0 = this.getV0();
         float v1 = this.getV1();
-        int light = this.getLightColor(partialTicks);
-        vertex(buffer, bottomLeft, u1, v1, light);
-        vertex(buffer, topLeft, u1, v0, light);
-        vertex(buffer, topRight, u0, v0, light);
-        vertex(buffer, bottomRight, u0, v1, light);
-        vertex(buffer, bottomRight, u0, v1, light);
-        vertex(buffer, topRight, u0, v0, light);
-        vertex(buffer, topLeft, u1, v0, light);
-        vertex(buffer, bottomLeft, u1, v1, light);
+        vertex(buffer, bottomLeft, u1, v1, light, alpha);
+        vertex(buffer, topLeft, u1, v0, light, alpha);
+        vertex(buffer, topRight, u0, v0, light, alpha);
+        vertex(buffer, bottomRight, u0, v1, light, alpha);
+        vertex(buffer, bottomRight, u0, v1, light, alpha);
+        vertex(buffer, topRight, u0, v0, light, alpha);
+        vertex(buffer, topLeft, u1, v0, light, alpha);
+        vertex(buffer, bottomLeft, u1, v1, light, alpha);
     }
 
     private void updateSprite(float partialTicks) {
@@ -109,10 +124,15 @@ public final class BloodTempestKatanaLightningStrikeParticle extends TextureShee
         this.setSprite(this.sprites.get(frame, FRAME_COUNT - 1));
     }
 
-    private void vertex(VertexConsumer buffer, Vector3f vertex, float u, float v, int light) {
+    private static float smoothStep(float value) {
+        float clamped = Mth.clamp(value, 0.0F, 1.0F);
+        return clamped * clamped * (3.0F - 2.0F * clamped);
+    }
+
+    private void vertex(VertexConsumer buffer, Vector3f vertex, float u, float v, int light, float alpha) {
         buffer.vertex(vertex.x(), vertex.y(), vertex.z())
                 .uv(u, v)
-                .color(1.0F, 1.0F, 1.0F, this.alpha)
+                .color(1.0F, 1.0F, 1.0F, alpha)
                 .uv2(light)
                 .endVertex();
     }
