9b065f7bc0ee0eecfeb78ac8b8635b92705a86c1
Saint <zackryan512@gmail.com>
2026-09-12 15:18:33 +0700

    MANE COME ON MANEEEE

--- common/src/main/java/com/leon/saintsdragons/common/config/SaintsDragonsConfig.java
+++ common/src/main/java/com/leon/saintsdragons/common/config/SaintsDragonsConfig.java
@@ -23,9 +23,9 @@ public final class SaintsDragonsConfig {
     public static final int STEGONAUT_MIN_GROUP_SIZE_DEFAULT = 1;
     public static final int STEGONAUT_MAX_GROUP_SIZE_DEFAULT = 4;
 
-    public static final int CINDERVANE_SPAWN_WEIGHT_DEFAULT = 10;
+    public static final int CINDERVANE_SPAWN_WEIGHT_DEFAULT = 1;
     public static final int CINDERVANE_MIN_GROUP_SIZE_DEFAULT = 1;
-    public static final int CINDERVANE_MAX_GROUP_SIZE_DEFAULT = 3;
+    public static final int CINDERVANE_MAX_GROUP_SIZE_DEFAULT = 2;
 
     public static final int ATROXIIA_SPAWN_WEIGHT_DEFAULT = 6;
     public static final int ATROXIIA_MIN_GROUP_SIZE_DEFAULT = 1;
